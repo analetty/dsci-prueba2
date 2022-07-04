0d4e54ef02ebd2f6172bd9bc3f4425d1300261e3
analetty <14-10515@usb.ve>
2022-07-04 15:36:57 -0400

    Defino pasos a seguir en la limpieza de datos

--- P2_ana-ibarra.R
+++ P2_ana-ibarra.R
@@ -18,13 +18,31 @@ library(ggplot2)
 # Importamos el archivo desde el directorio donde se encuentra
 
 getwd()
-#setwd("prueba2") #Directorio donde tenemos el archivo
-messy_data <- read_excel("ds-abpolicies.xlsx")
-messy_data2 <- read_excel("ds-abrate.xlsx")
-messy_data3 <- read_excel("ds-wntdprg.xlsx")
+#setwd("prueba2/dsci-prueba2/datasets") #Directorio donde tenemos el archivo
+
+dirty_abpolicies <- read_excel("ds-abpolicies.xlsx")
+
+dirty_abrate <- read_excel("ds-abrate.xlsx")
+
+dirty_wntdprg <- read_excel("ds-wntdprg.xlsx")
+
+names(dirty_abpolicies)[names(dirty_abpolicies) == 'Member State (States/Entities)'] <- 'Country'
+
+a = anti_join(dirty_abpolicies, dirty_abrate)
 
 
 
 # Parte 2: Limpieza de datos -------------------------------------------------------
+
+# Paso 1 identificar los países que estén diferentes en dirty_abpolicies y 
+# ponerlos como en los otros ds
+
+# Definir con qué observaciones nos vamos a quedar de dirty_abpolicies
+
+# Eliminar filas innecesarias dirty_abrate y dirty_wntdprg
+
+# Cambiar u por NA en dirty_abrate y dirty_wntdprg
+dirty_abrate[dirty_abrate == 'u'] <- NA
+dirty_wntdprg[dirty_wntdprg == 'u'] <- NA
 
 # Parte 3: Resumen y Graficos ------------------------------------------------------
